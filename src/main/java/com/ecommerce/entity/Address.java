265216b25cadc2458c15e183f8a8dd9925a77bba
supawadeek <latifetor@gmail.com>
2024-03-06 22:37:23 -0500

    E-Commerce Lab: create entity package

---
+++ src/main/java/com/ecommerce/entity/Address.java
@@ -0,0 +1,20 @@
+package com.ecommerce.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+public class Address extends BaseEntity{
+
+    private String name;
+    private String street;
+    private String zipCode;
+
+    @ManyToOne
+    private Customer customer;
+}
---
+++ src/main/java/com/ecommerce/entity/Balance.java
@@ -0,0 +1,20 @@
+package com.ecommerce.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+public class Balance extends BaseEntity {
+
+    private BigDecimal amount;
+
+    @OneToOne
+    private Customer customer;
+}
